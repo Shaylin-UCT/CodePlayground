44d57d3b7564b406339ce4a54141bb0f3b352b03
Shaylin-UCT <chtsha042@myuct.ac.za>
2023-05-09 19:46:18 +0200

    initial

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float cost = input.nextFloat();
+        System.out.println("Hello");
+        int items = input.nextInt();
+        char x = input.next().charAt(0);
+        System.out.println(cost + " : " + items + " : " + x);
+    }
+}
